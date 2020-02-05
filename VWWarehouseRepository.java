3864c0db6050343f1217cf531fea6b48ee5d868d
ww6684ni <60165855+snowywhitemn@users.noreply.github.com>
2020-02-05 11:01:16 -0600

    Add files via upload

--- VWWarehouseRepository.java
+++ VWWarehouseRepository.java
@@ -3,6 +3,12 @@ package edu.metrostate.ics372.snowywhitemn;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Singleton class to manage the different warehouse objects.
+ * 
+ * @author RANDOMNAME Group Project (https://github.com/f3z087/RANDOMNAME)
+ *
+ */
 public class VWWarehouseRepository {
 
 	private Map<String, VWWarehouse> whRepo = new HashMap<>();
@@ -12,6 +18,12 @@ public class VWWarehouseRepository {
 
 	}
 
+	/**
+	 * Get and instance of the warehouse repository. Will create a new instance if
+	 * one has not already been created.
+	 * 
+	 * @return warehouse repository instance
+	 */
 	public static VWWarehouseRepository getInstance() {
 		if (singleton == null) {
 			singleton = new VWWarehouseRepository();
@@ -19,6 +31,12 @@ public class VWWarehouseRepository {
 		return singleton;
 	}
 
+	/**
+	 * Gets the specified warehouse object from the warehouse repository map.
+	 * 
+	 * @param warehouseId
+	 * @return warehouse object
+	 */
 	public VWWarehouse getWarehouse(String warehouseId) {
 
 		if (whRepo.containsKey(warehouseId)) {
